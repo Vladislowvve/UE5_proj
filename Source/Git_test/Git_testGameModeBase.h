c854b8d26755ede428b2a14c12ec2e0bd65def7b
Vlad <wladkiryllo@wp.pl>
2023-04-24 15:45:17 +0300

    First commit

---
+++ Source/Git_test/Git_testGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Git_testGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GIT_TEST_API AGit_testGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
